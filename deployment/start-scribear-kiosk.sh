98f30741bf78cac5d5a314e9bab7381c9abb1973
mukherja04 <84088896+mukherja04@users.noreply.github.com>
2025-10-16 01:56:15 +0000

    Implement keyboard shortcut disabling in kiosk script
    Added functionality to disable keyboard shortcuts and clear modifier keys for kiosk mode.

--- deployment/start-scribear-kiosk.sh
+++ deployment/start-scribear-kiosk.sh
@@ -8,6 +8,26 @@
 
 SCRIBEAR_URL="https://<SCRIBEAR_ADDRESS>/?mode=kiosk&kioskServerAddress=<SCRIBEAR_ADDRESS>&sourceToken=CHANGEME&scribearURL=https://<SCRIBEAR_ADDRESS>"
 
+# Disable all shortcuts except basic typing using xmodmap and setxkbmap
+# Backup current keymap
+xmodmap -pke > "$BASE_DIR/original_keymap.xmodmap"
+setxkbmap -option
+xmodmap -e "clear control"
+xmodmap -e "clear mod1"
+xmodmap -e "clear mod2"
+xmodmap -e "clear mod3"
+xmodmap -e "clear mod4"
+xmodmap -e "clear mod5"
+
+# Manually unmap all common modifier key keycodes (Left/Right Ctrl, Alt, Super/Windows, Menu)
+for key in 37 105 64 108 133 134 135 50 62 66 77 92; do
+  xmodmap -e "keycode $key = NoSymbol"
+done
+# Disable Escape (9), Tab (23), and F1-F12 (67-96)
+for key in 9 23 67 68 69 70 71 72 73 74 75 76 95 96; do
+  xmodmap -e "keycode $key = NoSymbol"
+done
+
 # Loop until ScribeAR is reachable with curl
 curl -s $SCRIBEAR_URL >/dev/null
 while [ $? -ne 0 ]; do
