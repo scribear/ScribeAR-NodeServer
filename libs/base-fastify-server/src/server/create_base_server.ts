d4d2f96365895f9bfd51820f3953e33d3a9e2805
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-10-22 22:53:27 -0500

    feat(base-fastify-server): add custom json parser

--- libs/base-fastify-server/src/server/create_base_server.ts
+++ libs/base-fastify-server/src/server/create_base_server.ts
@@ -15,6 +15,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import type { BaseLogger, LogLevel } from './create_logger.js';
 import { createLogger } from './create_logger.js';
+import jsonParser from './plugins/json_parser.js';
 import schemaValidator from './plugins/schema_validator.js';
 import type { BaseDependencies } from './types/base_dependencies.js';
 
@@ -59,6 +60,7 @@ function createBaseServer(
   // Register plugins
   fastify.register(fastifySensible);
   fastify.register(fastifyHelmet);
+  fastify.register(jsonParser);
   fastify.register(schemaValidator);
 
   return {
