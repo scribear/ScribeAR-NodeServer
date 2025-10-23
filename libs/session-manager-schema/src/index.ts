11df1bae6fef0117e982d455f1f8bbc3a5c96aa9
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-10-22 22:17:20 -0500

    feat(session-manager-schema): create toy calculator schema to demo usage

--- libs/session-manager-schema/src/index.ts
+++ libs/session-manager-schema/src/index.ts
@@ -1,3 +1,21 @@
-import { HEALTHCHECK_SCHEMA } from './healthcheck/healthcheck.schema.js';
+import {
+  COMPUTE_BINOMIAL_ROUTE,
+  COMPUTE_BINOMIAL_SCHEMA,
+} from './calculator/compute_binomial.schema.js';
+import {
+  COMPUTE_MONOMIAL_ROUTE,
+  COMPUTE_MONOMIAL_SCHEMA,
+} from './calculator/compute_monomial.schema.js';
+import {
+  HEALTHCHECK_ROUTE,
+  HEALTHCHECK_SCHEMA,
+} from './healthcheck/healthcheck.schema.js';
 
-export { HEALTHCHECK_SCHEMA };
+export {
+  COMPUTE_BINOMIAL_SCHEMA,
+  COMPUTE_BINOMIAL_ROUTE,
+  COMPUTE_MONOMIAL_SCHEMA,
+  COMPUTE_MONOMIAL_ROUTE,
+  HEALTHCHECK_ROUTE,
+  HEALTHCHECK_SCHEMA,
+};
