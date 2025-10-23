637f58b6025175f2ce02512d05a5f6a139256e53
Bennett Wu <57691028+bennettrwu@users.noreply.github.com>
2025-10-22 23:32:39 -0500

    feat(session-manager): register app dependencies with container

--- apps/session-manager/src/server/create_server.ts
+++ apps/session-manager/src/server/create_server.ts
@@ -1,20 +1,25 @@
 import { createBaseServer } from '@scribear/base-fastify-server';
 
 import type AppConfig from '../app_config/app_config.js';
+import registerDependencies from './dependency_injection/register_dependencies.js';
 import swagger from './plugins/swagger.js';
 
 /**
- * Initializes fastify server
+ * Initializes fastify server and registers dependencies
  * @param config Application config
  * @returns Initialized fastify server
  */
 async function createServer(config: AppConfig) {
-  const { logger, fastify } = createBaseServer(config.logLevel);
+  const { logger, dependencyContainer, fastify } = createBaseServer(
+    config.logLevel,
+  );
 
   // Only include swagger docs if in development mode
   if (config.isDevelopment) {
     await fastify.register(swagger);
   }
+
+  registerDependencies(dependencyContainer, config);
 
   return { logger, fastify };
 }
---
+++ apps/session-manager/src/server/dependency_injection/register_dependencies.ts
@@ -0,0 +1,44 @@
+// Need to import so that declare module '@fastify/awilix' below works
+import '@fastify/awilix';
+import { type AwilixContainer, asValue } from 'awilix';
+
+import type { BaseDependencies } from '@scribear/base-fastify-server';
+
+import type AppConfig from '../../app_config/app_config.js';
+
+/**
+ * Define types for entities in dependency container
+ */
+interface AppDependencies extends BaseDependencies {
+  config: AppConfig;
+}
+
+/**
+ * Ensure fastify awilix container is typed correctly
+ * @see https://github.com/fastify/fastify-awilix?tab=readme-ov-file#typescript-usage
+ */
+declare module '@fastify/awilix' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Cradle extends AppDependencies {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface RequestCradle extends AppDependencies {}
+}
+
+/**
+ * Register all controller, service, and repository classes into dependency container
+ * @param dependencyContainer Container to load dependencies into
+ * @param config AppConfig to be registered into dependency controller
+ */
+function registerDependencies(
+  dependencyContainer: AwilixContainer,
+  config: AppConfig,
+) {
+  dependencyContainer.register({
+    // Config
+    config: asValue(config),
+  });
+}
+
+export default registerDependencies;
+export type { AppDependencies };
